c018f1ea7d7d1b2ec43ffaf3248fa592c3a0344e
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-11-01 17:51:58 +0500

    done

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center h-[100vh] text-5xl'>chai pila do</div>
+  )
+}
+
+export default page
